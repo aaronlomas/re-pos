f9b4ecc61151c31c141f21b54bcf1f696414b736
aaronlomas <161402338+aaronlomas@users.noreply.github.com>
2025-02-13 16:04:23 -0500

    Update confetti.js
    confetti modificado una vez

--- carta/public/js/confetti.js
+++ carta/public/js/confetti.js
@@ -77,8 +77,8 @@ var confetti = {
                 particle.y = height + 100;
             } else {
                 particle.tiltAngle += particle.tiltAngleIncrement;
-                particle.x += Math.sin(waveAngle) - 0.5;
-                particle.y += (Math.cos(waveAngle) + particle.size + confetti.speed) * 0.5;
+                particle.x += Math.sin(waveAngle) * 2;
+                particle.x += Math.sin(waveAngle) * 2 + (Math.random() - 0.5) * 3;
             }
             if (particle.x > width + 20 || particle.x < -20 || particle.y > height) {
                 if (streamingConfetti && particles.length <= confetti.maxCount)
